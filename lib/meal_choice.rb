2d13db45960ebc55978be71bae7513bd7647b902
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-26 19:39:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
 def meal_choice(vegetable,carb,protein="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{vegetable} and #{carb}."
+"A plate of #{protein} with #{vegetable} and #{carb}."
 end
